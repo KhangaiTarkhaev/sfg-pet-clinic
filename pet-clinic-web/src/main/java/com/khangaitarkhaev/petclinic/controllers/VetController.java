ea515b7167d24471f3016f012e1289f81ad04186
Khangai Tarkhaev <hangaitarhaev95@gmail.com>
2021-01-21 19:04:19 +0800

    Create Owner index and Controller Closes #15

--- pet-clinic-web/src/main/java/com/khangaitarkhaev/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/khangaitarkhaev/petclinic/controllers/VetController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listVets() {
         return "vets/index";
     }
