fc4163dc2ae4c9327dfee65a13545f819e86d24a
Khangai Tarkhaev <hangaitarhaev95@gmail.com>
2021-02-25 00:55:13 +0800

    Owner Controller and view

--- pet-clinic-web/src/main/java/com/khangaitarkhaev/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/khangaitarkhaev/petclinic/controllers/OwnerController.java
@@ -3,6 +3,8 @@ package com.khangaitarkhaev.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
