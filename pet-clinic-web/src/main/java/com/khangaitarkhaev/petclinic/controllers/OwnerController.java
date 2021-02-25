0298d98e6ea6a2c9de552f93b122912703cb9d39
Khangai Tarkhaev <hangaitarhaev95@gmail.com>
2021-02-25 22:19:06 +0800

    Added List of All Owners to owners/index.html. Closes #18

--- pet-clinic-web/src/main/java/com/khangaitarkhaev/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/khangaitarkhaev/petclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package com.khangaitarkhaev.petclinic.controllers;
 
+import com.khangaitarkhaev.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -9,8 +11,17 @@ import java.util.List;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
