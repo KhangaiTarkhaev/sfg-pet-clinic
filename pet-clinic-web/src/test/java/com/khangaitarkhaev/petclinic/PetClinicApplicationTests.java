383f7d1ed3dd3e3bf638a5f667e1151656293c8f
Khangai Tarkhaev <hangaitarhaev95@gmail.com>
2021-02-27 01:23:11 +0800

    Added i18n properties. Closes #29

--- pet-clinic-web/src/test/java/com/khangaitarkhaev/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/khangaitarkhaev/petclinic/PetClinicApplicationTests.java
@@ -1,11 +1,13 @@
 package com.khangaitarkhaev.petclinic;
 
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PetClinicApplicationTests {
 
+    @Test
     void contextLoads() {
     }
 
